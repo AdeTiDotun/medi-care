efc26dfafd045cacfef7bbfb2bcc3deb6e4faf54
Daniel <adejumodaniel274@gmail.com>
2024-07-18 22:45:37 +0100

    add strucutre of the project

---
+++ src/components/footer.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>medicare</h1>
+          <h3>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </h3>
+          <h3>social media logos and connects</h3>
+        </div>
+
+        <div>
+          <div>
+            <h2>Company</h2>
+            <ul>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Services</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <h2>Pages</h2>
+          <div>
+            <ul>
+              <li>Our Team</li>
+              <li>Pricing Plan</li>
+              <li>404 Page</li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <h2>Subscribe</h2>
+          <h2>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <div>Logo</div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>How it works</li>
+          <li>Contact us</li>
+          <li>Admin Login</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <button>log in</button>
+          </li>
+          <li>
+            <button>sign up</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Nav;
