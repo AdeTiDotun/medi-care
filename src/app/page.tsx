bdbf3b2f939146df97d3657a8c18da32cf5037fa
Daniel <adejumodaniel274@gmail.com>
2024-07-26 15:06:19 +0100

    how it works session frame added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    <h1>Home page</h1>
+      <div className="bg-white text-gray-500">
+        <div>
+          <h2 className="mt-8 mb-8">
+            Discover your perfect care, Health is Wealth
+          </h2>
+          <h1>Get the nearest Medical Care Providers, Save Time!</h1>
+          <button className="bg-green-500 rounded px-2 py-4">
+            Get started
+          </button>
+        </div>
+
+        <div></div>
+      </div>
+
+      {/* How it works' session */}
+      <div>
+        <div>
+          <h2>How it works</h2>
+        </div>
+        <div>
+          <div>
+
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
