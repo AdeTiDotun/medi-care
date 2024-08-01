5f7e954c2511de215020e9aad77fceae5818d39b
Daniel <adejumodaniel274@gmail.com>
2024-08-01 15:26:16 +0100

    main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
+import Main from "@/components/main";
+import Nav from "@/components/navbar";
 import Users from "@/components/users";
+
 import Link from "next/link";
 
 
 export default function Home() {
   return (
     <>
-      <div className="bg-white text-gray-500">
-        <div>
-          <h2 className="mt-8 mb-8">
-            Discover your perfect care, Health is Wealth
-          </h2>
-          <h1>Get the nearest Medical Care Providers, Save Time!</h1>
-          <button className="bg-green-500 rounded px-2 py-4">
-            Get started
-          </button>
-        </div>
-
-        <div></div>
-      </div>
+      <Nav />
+      <Main />
 
       {/* How it works' session */}
       <div>
@@ -25,9 +17,7 @@ export default function Home() {
           <h2>How it works</h2>
         </div>
         <div>
-          <div>
-
-          </div>
+          <div></div>
           <div></div>
           <div></div>
           <div></div>
---
+++ src/components/main.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Image from "next/image";
+function Main() {
+  return (
+    <>
+      <div className="text-gray-500 flex gap">
+        <div className="w-1/2 ">
+          <h2 className="ml-8 w-1/2 mt-8 mb-8 text-3xl py-14">
+            Discover your perfect care, Health is Wealth
+          </h2>
+          <h1 className="text-8xl ml-8 text-green-500 font-bold">
+            Get the nearest Medical Care Providers, Save Time!
+          </h1>
+
+          <div className="py-1">
+            <p className="ml-8  mt-8 mb-8 text-3xl">
+              Top-notch, First class medical services. Get fully integrative
+              care from well trained doctors and medical personels with
+              affordable subscription fee.
+            </p>
+          </div>
+
+          <div className="py-14">
+            <button className=" ml-8 border-green border-2 bg-green-500 rounded w-80 mx-auto py-5 text-center text-white text-2xl hover:bg-white hover:text-black ">
+              Find Your Hospital
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <Image
+            src="/Images/homealt.jpg"
+            alt="known"
+            width={900}
+            height={300}
+          ></Image>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main;
