70f9e98bf9dc55c23bde902070fb3befd07f0db1
Daniel <adejumodaniel274@gmail.com>
2024-08-15 09:03:33 +0100

    new update on signUp page

--- src/app/sign_in/page.tsx
+++ src/app/sign_in/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react';
 import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth'
 import { auth } from "../../../firebase/config";
-import React from "react";
 import Link from "next/link";
 import { useRouter } from 'next/navigation';
 
